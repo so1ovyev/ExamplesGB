912bba497228733ab23ee6942fcf36318d00e71d
solovyev <solovyev2019@gmail.com>
2022-12-15 22:50:01 +0300

    exampl005_greetings

---
+++ Exampl005_Greetings/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string  username = Console.ReadLine();
+
+if (username.ToLower() == "artem")
+{
+    Console.WriteLine("Salam, Artem!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}

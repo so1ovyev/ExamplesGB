e3e6310ac859213039af6cf8d8c3d3922acb0de2
solovyev <solovyev2019@gmail.com>
2022-12-16 18:05:14 +0300

    Exapml008_maximum_spontaneously

---
+++ Exapml008_maximum_spontaneously/Program.cs
@@ -0,0 +1,14 @@
+﻿int a1 = 15, b1 = 23, c1 = 33, 
+    a2 = 154, b2 = 232, c2 = 331, 
+    a3 = 17, b3 = 26, c3 = 39;
+int max = a1;
+if (b1 > max) max = b1;
+if (c1 > max) max = c1;
+if (a2 > max) max = a2;
+if (b2 > max) max = b2;
+if (c2 > max) max = c2;
+if (a3 > max) max = a3;
+if (b3 > max) max = b3;
+if (c3 > max) max = c3;
+
+Console.WriteLine (max);
